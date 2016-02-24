cc858c690836288f3b3dcd9abb953bc8bd56864e
HAYASHI Kentaro <kenhys@gmail.com>
2016-02-25 02:56:27 +0900

    Add theme

---
+++ theme.rb
@@ -0,0 +1,25 @@
+#@title_slide_title_font_size = @small_font_size
+
+include_theme("clear-code")
+
+@slide_logo_image = "openlogo-nd-50.png"
+include_theme("clear-blue")
+@icon_images = ["openlogo-nd-75.png"]
+include_theme("icon")
+
+#background_image = "openlogo-nd.svg"
+#@slide_background_image = background_image
+#@title_slide_background_image = background_image
+#@slide_background_image_relative_width = 50
+#include_theme("slide-background-image")
+#include_theme("title-slide-background-image")
+
+@image_slide_number_image = "openlogo-nd-50.png"
+@image_timer_image = "openlogo-nd-50.png"
+#@block_quote_open_quote_image = "open-quote-sakana.png"
+#@block_quote_close_quote_image = "close-quote-sakana.png"
+
+
+#include_theme("yart")
+#include_theme("sprk2012")
+#include_theme("nyankosakana")
